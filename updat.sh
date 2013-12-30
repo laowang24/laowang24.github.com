56e71918e25c0012e2d6b62fdf82606361d1f48e
子优 <zhipeng.wangzp@alibaba-inc.com>
2013-12-30 21:26:23 +0800

    update at 2013-12-30

--- updat.sh
+++ updat.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 date=`date +%F`
-git add . && git commit -m "update at $date" && git push git@github.com:laowang24/laowang24.github.com.git 
+git add . && git commit -m "update at $date" && git push origin master
